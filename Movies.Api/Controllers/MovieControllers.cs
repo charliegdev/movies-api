9fd03ac25ecc5b2073aa20a3ac5cd4182e39d529
charliegdev <charliegdev@gmail.com>
2026-05-30 11:13:51 -0700

    Add MovieController with dependency injection for movie repository

---
+++ Movies.Api/Controllers/MovieControllers.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Movies.Application.Repositories;
+
+namespace Movies.Api.Controllers;
+
+[ApiController]
+public class MovieController : ControllerBase
+{
+    private readonly IMovieRepository _movieRepository;
+
+    public MovieController(IMovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+}
