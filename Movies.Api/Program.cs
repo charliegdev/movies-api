92873475ae258f30ac0e7d9a33be491a26b1b254
charliegdev <charliegdev@gmail.com>
2026-06-07 11:44:59 -0700

    Add API versioning support to Movies.Api
    - Introduced Asp.Versioning.Mvc package to enable API versioning.
    - Configured default API versioning settings in Program.cs.
    - Annotated MoviesController with ApiVersion attribute to specify version 1.0.

--- Movies.Api/Program.cs
+++ Movies.Api/Program.cs
@@ -1,4 +1,5 @@
 using System.Text;
+using Asp.Versioning;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Movies.Api.Auth;
@@ -51,6 +52,16 @@ builder.Services.AddAuthorization(x =>
 builder.Services.AddOpenApi();
 
 builder.Services.AddApplication();
+
+builder
+    .Services.AddApiVersioning(x =>
+    {
+        x.DefaultApiVersion = new ApiVersion(1.0);
+        x.AssumeDefaultVersionWhenUnspecified = true;
+        x.ReportApiVersions = true;
+        x.ApiVersionReader = new MediaTypeApiVersionReader("api-version");
+    })
+    .AddMvc();
 builder.Services.AddControllers();
 builder.Services.AddDatabase(config["Database:ConnectionString"]!);
 
